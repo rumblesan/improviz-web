197fd4709bbc3ed7cf6eaeb8697c8255ca14a956
Guy John <guy@rumblesan.com>
2020-03-02 08:22:35 +0000

    Set testing mode for parser

--- test/helpers.js
+++ test/helpers.js
@@ -4,7 +4,7 @@ import assert from 'assert';
 
 global.parserTest = function(parser, program, expectedAst) {
   return () => {
-    var { ast, errors } = parser.parse(program);
+    var { ast, errors } = parser.parse(program, { testing: true });
     assert.deepEqual(errors, []);
     assert.deepEqual(ast, expectedAst);
   };
