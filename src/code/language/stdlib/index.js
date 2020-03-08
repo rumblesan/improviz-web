9f44c79f429553026c5fbf31fea4407e03ebced2
Guy John <guy@rumblesan.com>
2020-03-08 13:40:24 +0000

    Adding maths functions

--- src/code/language/stdlib/index.js
+++ src/code/language/stdlib/index.js
@@ -3,6 +3,7 @@ import { makeFunc, makeNum } from '../ffi';
 import { style, strokeSize } from './style';
 import { shape } from './shapes';
 import { matrix } from './transforms';
+import * as maths from './maths';
 import {
   pushSnapshot,
   popSnapshot,
@@ -36,6 +37,17 @@ export class StdLib {
       background: makeFunc('background', background.bind(this.runtime)),
       depthOff: makeFunc('depthOff', depthOff.bind(this.runtime)),
       isNull: makeFunc('isNull', isNull.bind(this.runtime)),
+      sin: makeFunc('sin', maths.sin.bind(this.runtime)),
+      cos: makeFunc('cos', maths.cos.bind(this.runtime)),
+      tan: makeFunc('tan', maths.tan.bind(this.runtime)),
+      abs: makeFunc('abs', maths.abs.bind(this.runtime)),
+      ceil: makeFunc('ceil', maths.ceil.bind(this.runtime)),
+      floor: makeFunc('floor', maths.floor.bind(this.runtime)),
+      round: makeFunc('round', maths.round.bind(this.runtime)),
+      max: makeFunc('max', maths.max.bind(this.runtime)),
+      min: makeFunc('min', maths.min.bind(this.runtime)),
+      log: makeFunc('log', maths.log.bind(this.runtime)),
+      sqrt: makeFunc('sqrt', maths.sqrt.bind(this.runtime)),
       animationStyle: makeFunc(
         'animationStyle',
         animationStyle.bind(this.runtime)
---
+++ src/code/language/stdlib/maths.js
@@ -0,0 +1,93 @@
+import { makeNum } from '../ffi';
+import { InterpreterError } from '../interpreter/errors';
+import { isNum } from '../ffi';
+
+export function sin(args) {
+  const angle = args[0];
+  if (!isNum(angle))
+    throw new InterpreterError(
+      `Expected Number but found ${angle.type}`,
+      angle
+    );
+  return makeNum(Math.sin(angle.value));
+}
+
+export function cos(args) {
+  const angle = args[0];
+  if (!isNum(angle))
+    throw new InterpreterError(
+      `Expected Number but found ${angle.type}`,
+      angle
+    );
+  return makeNum(Math.cos(angle.value));
+}
+
+export function tan(args) {
+  const angle = args[0];
+  if (!isNum(angle))
+    throw new InterpreterError(
+      `Expected Number but found ${angle.type}`,
+      angle
+    );
+  return makeNum(Math.tan(angle.value));
+}
+
+export function abs(args) {
+  const val = args[0];
+  if (!isNum(val))
+    throw new InterpreterError(`Expected Number but found ${val.type}`, val);
+  return makeNum(Math.abs(val.value));
+}
+
+export function ceil(args) {
+  const val = args[0];
+  if (!isNum(val))
+    throw new InterpreterError(`Expected Number but found ${val.type}`, val);
+  return makeNum(Math.ceil(val.value));
+}
+
+export function floor(args) {
+  const val = args[0];
+  if (!isNum(val))
+    throw new InterpreterError(`Expected Number but found ${val.type}`, val);
+  return makeNum(Math.floor(val.value));
+}
+
+export function round(args) {
+  const val = args[0];
+  if (!isNum(val))
+    throw new InterpreterError(`Expected Number but found ${val.type}`, val);
+  return makeNum(Math.round(val.value));
+}
+
+export function log(args) {
+  const val = args[0];
+  if (!isNum(val))
+    throw new InterpreterError(`Expected Number but found ${val.type}`, val);
+  return makeNum(Math.log(val.value));
+}
+
+export function sqrt(args) {
+  const val = args[0];
+  if (!isNum(val))
+    throw new InterpreterError(`Expected Number but found ${val.type}`, val);
+  return makeNum(Math.sqrt(val.value));
+}
+
+export function max(args) {
+  const [v1, v2] = args;
+  if (!isNum(v1))
+    throw new InterpreterError(`Expected Number but found ${v1.type}`, v1);
+  if (!isNum(v2))
+    throw new InterpreterError(`Expected Number but found ${v2.type}`, v2);
+  return v1.value > v2.value ? v1 : v2;
+}
+
+export function min(args) {
+  const [v1, v2] = args;
+  if (!isNum(v1))
+    throw new InterpreterError(`Expected Number but found ${v1.type}`, v1);
+  if (!isNum(v2))
+    throw new InterpreterError(`Expected Number but found ${v2.type}`, v2);
+  return v1.value < v2.value ? v1 : v2;
+}
